66508b01ed02254a52fc870e778633af5e89e581
ktrain10298765 <68798186+ktrain10298765@users.noreply.github.com>
2022-07-10 20:29:24 -0400

    remove sketchy thing #2

--- Source/Calvy/SkiBlox/api.cs
+++ Source/Calvy/SkiBlox/api.cs
@@ -6,7 +6,6 @@ namespace SkiBlox
 	{
 		public static string apihost = Globals.host + "/api";
 		public static string getuser = api.apihost + "/getuser.php";
-		public static string getip = api.apihost + "/getip.php";
 		public static string id = api.apihost + "/getid.php";
 		public static string getversion = "http://calvyy.xyz/api/download/version.txt";
 		public static string login = api.apihost + "/login.php";
